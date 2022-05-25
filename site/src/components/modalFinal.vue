8397c688db6bc151b0ab9c98b2db9e07f635b86d
Bruno  Henrique Brandl <74260274+bruno21637@users.noreply.github.com>
2022-05-25 16:55:58 -0300

    Update modalFinal.vue

--- site/src/components/modalFinal.vue
+++ site/src/components/modalFinal.vue
@@ -283,8 +283,7 @@ export default {
   width: 100%; 
   height: 100%;
   overflow: auto; 
-  background-color: rgb(0,0,0); 
-  background-color: rgba(0,0,0,0.4); 
+  background-color: rgba(0, 0, 0, .8);
 }
 .modal-content2 {
   margin: 0 auto;
